8da31023cc7769230c350967aa42ca16d16f0270
wei.miao <wei.miao@rograndec.com>
2018-12-07 17:58:06 +0800

    修改路径

--- AndroidZIP/src/com/example/androidzip/MainActivity.java
+++ AndroidZIP/src/com/example/androidzip/MainActivity.java
@@ -54,23 +54,23 @@ public class MainActivity extends Activity {
 
 				try {
 					//测试数据，注意更换目录
-					LinkedList<File> files = DirTraversal.listLinkedFiles(pathString+"/Android/data/com.mapbar.info.collection/files/cache");
+					LinkedList<File> files = DirTraversal.listLinkedFiles(pathString+"/Android/data/com.info.collection/files/cache");
 					
-					File file = DirTraversal.getFilePath(pathString+"/Android/data/com.mapbar.info.collection/files/", "cache.zip");
+					File file = DirTraversal.getFilePath(pathString+"/Android/data/com.info.collection/files/", "cache.zip");
 					
 					ZipUtils.zipFiles(files, file);
 					
 					//第二种实现
-					//ZipUtils.zip(pathString+"/Android/data/com.mapbar.info.collection/files/cache", pathString+"/Android/data/com.mapbar.info.collection/files/cache.zip");
-					//ZipUtils.unzip(pathString+"/Android/data/com.mapbar.info.collection/files/cache.zip", pathString+"/Android/data/com.mapbar.info.collection/files/cache");
+					//ZipUtils.zip(pathString+"/Android/data/com.info.collection/files/cache", pathString+"/Android/data/com.info.collection/files/cache.zip");
+					//ZipUtils.unzip(pathString+"/Android/data/com.info.collection/files/cache.zip", pathString+"/Android/data/com.info.collection/files/cache");
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 				break;
 			case R.id.btn_unzip:
-				File file = DirTraversal.getFilePath(pathString+"/Android/data/com.mapbar.info.collection/files/", "cache.zip");
+				File file = DirTraversal.getFilePath(pathString+"/Android/data/com.info.collection/files/", "cache.zip");
 				try {
-					ZipUtils.upZipFile(file, pathString+"/Android/data/com.mapbar.info.collection/files/cachezip");
+					ZipUtils.upZipFile(file, pathString+"/Android/data/com.info.collection/files/cachezip");
 				} catch (ZipException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
